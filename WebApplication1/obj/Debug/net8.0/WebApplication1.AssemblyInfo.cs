512ba87549c6e5c5db4d042c4b571979a7acc737
marci <marcin123523@wp.pl>
2024-12-02 18:40:12 +0100

    stary kod

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d58915c75d7f711a8815afb28dbfe99fc2b49224")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14aa51385f0a2055bb9e1ee8300a39f51d3ece32")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
