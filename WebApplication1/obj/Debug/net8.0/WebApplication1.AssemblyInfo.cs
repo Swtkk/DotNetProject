2c7efc3edcdba73d128654b1170bf7a1b66464e5
marci <marcin123523@wp.pl>
2024-12-07 14:38:28 +0100

    zarzadzanie wiadomosciami

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59e3590f7014acbb4eaf0f4bd02e8b2cbf89e031")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33caa8c6544f5891fea6ea4135e55794c5da3835")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
