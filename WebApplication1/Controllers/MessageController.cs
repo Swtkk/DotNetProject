550c602cb4278032b06a34beff4ae9e116a61f29
marci <marcin123523@wp.pl>
2024-12-07 14:32:50 +0100

    dolaczono zarzadzanie wiadomosciami oraz zalacznikami

---
+++ WebApplication1/Controllers/MessageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers;
+
+public class MessageController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
