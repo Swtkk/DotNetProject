fd944f3664acd5c25f9a45878df7771f76170315
marci <marcin123523@wp.pl>
2024-12-07 10:45:37 +0100

    ostatnie modyfikacje z rolami i uzytkownikami

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54637152f85c54e5e01134bad039898c786dd258")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9caac1df24faca4c0daa854a589e3d2cb944192")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
